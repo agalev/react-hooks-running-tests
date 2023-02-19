4d1531604edb9227f8a6365fa3179a3f4d387077
Alexander <alex.galev@yahoo.com>
2023-02-19 11:33:55 -0700

    passed tests

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,13 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import React from "react";
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import React from 'react'
 
-import Article from "../components/Article";
+import Article from '../components/Article'
 
 test("displays the text 'please pass this test'", () => {
-  render(<Article />);
-
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
-});
+	render(<Article />)
+  // const element = screen.getByText('please pass this test');
+	const element = screen.queryByText('please pass this test');
+	screen.debug(element)
+	expect(screen.queryByText('please pass this test')).toBeInTheDocument()
+})
